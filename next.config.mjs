9185c36113c32f85090ac377249e650f8f791f48
Abej-Rijwi <abejrijwicareers@gmail.com>
2025-03-17 18:49:33 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
